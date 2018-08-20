1c8ccf40e157a0c1ece88e045f8f68d5d08728d7
zhangmin <min.zhang-2@jia007.com>
2018-08-20 11:55:29 +0800

    spring国际化资源文件方式

--- spring-annotation/src/main/java/day44springioc/App.java
+++ spring-annotation/src/main/java/day44springioc/App.java
@@ -2,16 +2,8 @@ package day44springioc;
 
 import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import day44springioc.ApplicationListener.My02ApplicationListener;
-import day44springioc.ApplicationListener.MyApplicationEvent;
-import day44springioc.BeanPostProcessor.Person;
-import day44springioc.ResourceBundleMessageSource.MyResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 
 public class App {
@@ -20,14 +12,14 @@ public class App {
 		/**
 		 * 包扫描方式测试spring容器加载顺序
 		 */
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Myconfig.class);
+		/*AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Myconfig.class);
 		context.addApplicationListener(new My02ApplicationListener());
 		context.publishEvent(new MyApplicationEvent("init-bean"));
 		Dog Dog = context.getBean(Dog.class);
 		context.getBean(Dog.class);
 		Dog.jiao();
 		Person Person = context.getBean(Person.class);
-		Person.jiao();
+		Person.jiao();*/
 		
 		
 		/**
@@ -35,21 +27,22 @@ public class App {
 		 */
 		/*System.out.println("-----------测试资源文件国际化--------------");
 		ApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
-		MessageSource messageSource = applicationContext.getBean(MessageSource.class);
-		String msgUs  = messageSource.getMessage("foo.test", new Object[]{"jerry,{one}"}, Locale.US);
+		ResourceBundleMessageSource messageSource = applicationContext.getBean(ResourceBundleMessageSource.class);
+		String msgUs  = messageSource.getMessage("foo.test", new Object[]{"jerry"}, Locale.US);
 		System.out.println(msgUs);
-		String msgChines  = messageSource.getMessage("foo.test", new Object[]{"小明,{1}"}, Locale.CHINESE);
+		String msgChines  = messageSource.getMessage("foo.test", new Object[]{"小明"}, Locale.CHINESE);
 		System.out.println(msgChines);*/
 		
 		/**
 		 * 
 		 *  注解方式 测试资源文件国际化
 		 */
-		/*MyResourceBundleMessageSource messageSource = context.getBean(MyResourceBundleMessageSource.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Myconfig.class);
+		ResourceBundleMessageSource messageSource = context.getBean(ResourceBundleMessageSource.class);
 		messageSource.setBasenames("test_en_US","test_zh_CN");
 		String msgUs  = messageSource.getMessage("foo.test", new Object[]{"jerry"}, Locale.US);
 		System.out.println(msgUs);
 		String msgChines  = messageSource.getMessage("foo.test", new Object[]{"小明"}, Locale.CHINESE);
-		System.out.println(msgChines);*/
+		System.out.println(msgChines);
 	}
 }
--- spring-annotation/src/main/java/day44springioc/ResourceBundleMessageSource/MyResourceBundleMessageSource.java
+++
@@ -1,9 +0,0 @@
-package day44springioc.ResourceBundleMessageSource;
-
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyResourceBundleMessageSource extends ResourceBundleMessageSource {
-
-}
